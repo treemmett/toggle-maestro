493fe74d9d0a0d9c414a32387ed1b1fca37d528f
Tregan <tregan@tregan.me>
2022-10-14 18:45:56 -0400

    feat(manifest): support multiple flags

--- src/pages/api/flag.ts
+++ src/pages/api/flag.ts
@@ -1,7 +1,11 @@
+import axios from 'axios';
 import { Octokit } from 'octokit';
 import { nc } from '../../utils/nc';
 
 export class AuthorizationError extends Error {}
+export class ConflictError extends Error {}
+
+const MANIFEST_NAME = 'toggle-maestro-manifest.json';
 
 export default nc().post(async (req, res) => {
   const token = req.headers.authorization?.split(' ')?.[1];
@@ -9,15 +13,44 @@ export default nc().post(async (req, res) => {
     throw new AuthorizationError();
   }
 
+  const { id } = req.body;
+
   const github = new Octokit({ auth: token });
-  await github.rest.gists.create({
+
+  // check if we have an existing manifest
+  const gists = await github.rest.gists.list();
+  const manifest = gists.data.find((gist) => gist.description === MANIFEST_NAME);
+  if (!manifest || !manifest.files[MANIFEST_NAME].raw_url) {
+    await github.rest.gists.create({
+      description: MANIFEST_NAME,
+      files: {
+        [MANIFEST_NAME]: {
+          content: JSON.stringify({
+            [id]: false,
+          }),
+        },
+      },
+    });
+
+    res.send('ok');
+    return;
+  }
+
+  const { data } = await axios.get(manifest.files[MANIFEST_NAME].raw_url);
+
+  if (typeof data[id] !== 'undefined') {
+    throw new ConflictError();
+  }
+
+  data[id] = false;
+
+  await github.rest.gists.update({
     files: {
-      flag: {
-        content: JSON.stringify({
-          [req.body.id]: false,
-        }),
+      [MANIFEST_NAME]: {
+        content: JSON.stringify(data),
       },
     },
+    gist_id: manifest.id,
   });
 
   res.send('ok');
