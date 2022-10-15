12a2de83f62a2c3ab2c82e5257403da0fcd5acf5
Tregan <tregan@tregan.me>
2022-10-15 01:25:14 -0400

    feat(manifest): add get method

--- src/pages/api/manifest.ts
+++ src/pages/api/manifest.ts
@@ -3,13 +3,22 @@ import { nc } from '../../utils/nc';
 
 export class AuthorizationError extends Error {}
 
-export default nc().post(async (req, res) => {
-  const { id } = req.body;
-  if (!req.session) {
-    throw new AuthorizationError();
-  }
+export default nc()
+  .get(async (req, res) => {
+    if (!req.session) {
+      throw new AuthorizationError();
+    }
 
-  const manifest = await Manifest.getManifest(req.session);
-  manifest.createFlag(id);
-  res.send(manifest);
-});
+    const manifest = await Manifest.getManifest(req.session);
+    res.send(manifest);
+  })
+  .post(async (req, res) => {
+    const { id } = req.body;
+    if (!req.session) {
+      throw new AuthorizationError();
+    }
+
+    const manifest = await Manifest.getManifest(req.session);
+    manifest.createFlag(id);
+    res.send(manifest);
+  });
