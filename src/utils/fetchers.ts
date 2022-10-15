b819fe82f0f38b2daa977654866db25e659af9c8
Tregan <tregan@tregan.me>
2022-10-15 02:50:42 -0400

    feat(list): add optimistic update

--- src/utils/fetchers.ts
+++ src/utils/fetchers.ts
@@ -1,22 +1,40 @@
+import { plainToClass } from 'class-transformer';
+import { useCallback } from 'react';
 import useSWR from 'swr';
-import type { Manifest } from '../entities/Manifest';
+import { Manifest } from '../entities/Manifest';
 import { client } from './apiClient';
 
 export const useManifest = () => {
   const { data, error, mutate } = useSWR('manifest', async () => {
     const response = await client.get<Manifest>('/manifest');
-    return response.data;
+    return plainToClass(Manifest, response.data);
   });
+
+  const updateFlag = useCallback(
+    async (flag: string, enabled: boolean) => {
+      const clone = plainToClass(Manifest, data);
+      const optimisticData = clone.updateFlag(flag, enabled);
+
+      await mutate(
+        async () => {
+          const updated = await client.patch<Manifest>(`/manifest/${encodeURIComponent(flag)}`, {
+            enabled,
+          });
+          return plainToClass(Manifest, updated.data);
+        },
+        {
+          optimisticData,
+          revalidate: false,
+          rollbackOnError: true,
+        }
+      );
+    },
+    [data, mutate]
+  );
 
   return {
     data,
     error,
-    async updateFlag(flag: string, enabled: boolean) {
-      const manifest = await client.patch<Manifest>(`/manifest/${encodeURIComponent(flag)}`, {
-        enabled,
-      });
-
-      await mutate(manifest.data);
-    },
+    updateFlag,
   };
 };
