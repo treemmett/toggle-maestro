c6955ea821e2024eed096ad4ddbbbae0b3f942c4
Tregan <tregan@tregan.me>
2022-10-16 23:32:14 -0400

    build(server): add svg support

--- packages/server/next.config.js
+++ packages/server/next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  /**
+   * @param {import('webpack').Configuration} c
+   */
+  webpack: (c) => {
+    c.module.rules.push({
+      issuer: { and: [/\.(js|ts)x?$/] },
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    });
+    return c;
+  },
 };
 
 module.exports = nextConfig;
