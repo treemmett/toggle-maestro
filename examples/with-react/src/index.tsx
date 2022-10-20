85090589ff31e4e11ed6edfd1f00a063e834c7a0
Tregan <tregan@tregan.me>
2022-10-19 23:34:28 -0400

    docs(react example): enable extension hook

--- examples/with-react/src/index.tsx
+++ examples/with-react/src/index.tsx
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <MaestroProvider accessToken={process.env.REACT_APP_MAESTRO_KEY as string}>
+    <MaestroProvider accessToken={process.env.REACT_APP_MAESTRO_KEY as string} enableExtension>
       <App />
     </MaestroProvider>
   </React.StrictMode>
